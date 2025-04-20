27e664fa0754c4ceb7c5d817e06599de0119d5d8
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2025-04-20 17:17:33 +0200

    meta: og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,16 @@ import Nav from "@/src/ui/nav";
 import Footer from "@/src/ui/footer";
 // Main layout component
 export default function RootLayout({children}: { children: React.ReactNode; }) {
-
   return (
     <html>
+      <head>
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content="CNL - Collier Connecté" />
+        <meta property="og:description" content="Découvrez le collier connecté CNL, une solution innovante pour le bien-être de votre animal." />
+        <meta property="og:image" content="/collar.png" />
+        <meta property="og:url" content="https://cnl.nderousseaux.fr" />
+        <meta property="og:type" content="website" />
+      </head>
       <body className={`${primaryFont.className} antialiased p-4 min-h-screen flex flex-col gap-4`}>
         <Nav />
         {children}
