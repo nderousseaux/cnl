b6d90f578eb344e2b6824c1c405b6c426fafd79a
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2025-04-20 17:43:55 +0200

    📝 meta: adding metadaa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,30 @@
-
 import "@/src/ui/global.css";
 import { primaryFont } from '@/src/ui/fonts';
 
 import Nav from "@/src/ui/nav";
 import Footer from "@/src/ui/footer";
+
+// Metadata object
+export const metadata = {
+  title: "CNL - Collier Connecté",
+  description: "Découvrez le collier connecté CNL, une solution innovante pour le bien-être de votre animal.",
+  openGraph: {
+    title: "CNL - Collier Connecté",
+    description: "Découvrez le collier connecté CNL, une solution innovante pour le bien-être de votre animal.",
+    images: "/collar.png",
+    url: "https://cnl.nderousseaux.fr",
+    type: "website",
+  },
+};
+
 // Main layout component
-export default function RootLayout({children}: { children: React.ReactNode; }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
-      <head>
-        {/* Open Graph Meta Tags */}
-        <meta property="og:title" content="CNL - Collier Connecté" />
-        <meta property="og:description" content="Découvrez le collier connecté CNL, une solution innovante pour le bien-être de votre animal." />
-        <meta property="og:image" content="/collar.png" />
-        <meta property="og:url" content="https://cnl.nderousseaux.fr" />
-        <meta property="og:type" content="website" />
-      </head>
       <body className={`${primaryFont.className} antialiased p-4 min-h-screen flex flex-col gap-4`}>
         <Nav />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
